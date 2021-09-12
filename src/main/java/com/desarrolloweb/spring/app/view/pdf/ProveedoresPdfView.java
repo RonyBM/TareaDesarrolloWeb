a9a6d33f0be30933ffd592717a72eefe5dd9049a
RonyBM <ronyandree99@gmail.com>
2021-09-12 08:57:48 -0600

    Creacion de interfaz, controlador y demás ficheros de proveedores

--- src/main/java/com/desarrolloweb/spring/app/view/pdf/ProveedoresPdfView.java
+++ src/main/java/com/desarrolloweb/spring/app/view/pdf/ProveedoresPdfView.java
@@ -1,5 +1,35 @@
 package com.desarrolloweb.spring.app.view.pdf;
 
-public interface ProveedoresPdfView {
+import java.awt.Color;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.document.AbstractPdfView;
+
+import com.desarrolloweb.spring.app.entities.Proveedores;
+import com.lowagie.text.Document;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+
+@Component("detalle-cliente-form.pdf")
+public class ProveedoresPdfView extends AbstractPdfView {
+
+	@Override
+	protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer,
+			HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		Proveedores proveedor = (Proveedores) model.get("proveedor");
+		
+		PdfPTable tabla = new PdfPTable(1);
+		tabla.setSpacingAfter(20);
+		
+		tabla.addCell(proveedor.getNombre());
+		tabla.addCell(proveedor.getTelefono());
+		
+		document.add(tabla);
+	}
 
 }
